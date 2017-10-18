98f4fa3c45ed8f6cd9a1f98985bd761e1aae953a
Amy Churchwell and Chris Johnson <chris@>
2017-10-18 13:56:20 -0700

    add forms for all math functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
 
 $('document').ready(function(){
+  console.log('page loaded');
   $('#doAdd').submit(function(event){
-    debugger;
     var num1 = parseInt($('#number1').val());
     var num2 = parseInt($('#number2').val());
     var answer = num1 + num2;
+    $('#result').text(answer);
+    event.preventDefault();
+    //addition(num1, num2);
+  });
+  $('#doSub').submit(function(event){
+    console.log('TESTIN');
+    var num1 = parseInt($('#number1S').val());
+    var num2 = parseInt($('#number2S').val());
+    var answer = num1 - num2;
+    $('#result').text(answer);
+    event.preventDefault();
+    //addition(num1, num2);
+  });
+  $('#doMulti').submit(function(event){
+    console.log('TESTIN');
+    var num1 = parseInt($('#number1M').val());
+    var num2 = parseInt($('#number2M').val());
+    var answer = num1 * num2;
+    $('#result').text(answer);
+    event.preventDefault();
+    //addition(num1, num2);
+  });
+  $('#doDivide').submit(function(event){
+    console.log('TESTIN');
+    var num1 = parseInt($('#number1D').val());
+    var num2 = parseInt($('#number2D').val());
+    var answer = num1 / num2;
     $('#result').text(answer);
     event.preventDefault();
     //addition(num1, num2);
