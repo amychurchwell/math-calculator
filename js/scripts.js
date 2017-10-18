4a529742c92e436a63f27ff51865e5d9af8b39e2
Amy Churchwell and Chris Johnson <chris@>
2017-10-18 13:00:09 -0700

    add version before branch to test form submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
 
 $('document').ready(function(){
-  var answer;
-  console.log(answer);
-
   $('#doAdd').submit(function(event){
+    debugger;
     var num1 = parseInt($('#number1').val());
     var num2 = parseInt($('#number2').val());
-    answer = num1 + num2;
-    console.log(answer);
+    var answer = num1 + num2;
+    $('#result').text(answer);
     event.preventDefault();
     //addition(num1, num2);
   });
